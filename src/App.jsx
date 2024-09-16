e87e68d91bea050f841bce582549a66f91c8ad26
suraj <surajsk8558@gmail.com>
2024-09-16 20:10:20 +0530

    Completed project section

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-
-import './App.css' 
-import NavBar from './components/header/NavBar';
-import Hero from './components/Hero';
-import About from './components/About';
+import "./App.css";
+import NavBar from "./components/header/NavBar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Project from "./components/Project";
 
 function App() {
   return (
     <>
-      <main className='max-w-7xl mx-auto'>
+      <main className="max-w-7xl mx-auto">
         <NavBar />
         <Hero />
         <About />
-        </main>
+        <Project />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
